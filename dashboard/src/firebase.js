7d20505ede8c01958c0a415b630099b17a6a0368
Cihan Hartamaci <cihan.hartamaci@logiwa.com>
2026-02-20 12:13:22 -0600

    docs: finalize security guide and task tracking

--- dashboard/src/firebase.js
+++ dashboard/src/firebase.js
@@ -4,12 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // TODO: Replace with your actual Firebase config from the Firebase Console
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyDZX1Mujl9UK7GT8EDUezs0rHVMCexHJic",
+    authDomain: "logiwa-intelligence.firebaseapp.com",
+    projectId: "logiwa-intelligence",
+    storageBucket: "logiwa-intelligence.firebasestorage.app",
+    messagingSenderId: "802197643521",
+    appId: "1:802197643521:web:81ed8461d1e4661b93fb00",
+    measurementId: "G-7ZJ5LKZFL0"
 };
 
 const app = initializeApp(firebaseConfig);
